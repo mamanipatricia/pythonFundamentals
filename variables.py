3f18acbadc0e9bd691150c7a89080eac14285002
mamanipatricia <patttymq@gmail.com>
2020-02-27 21:01:58 -0400

    part1. variables

---
+++ variables.py
@@ -0,0 +1,17 @@
+# declaring variables
+a = 5
+b = 10
+
+my_variable = 56
+any_variable_name = 10
+
+string_variable = "Hello" # doble y simple quotes, there is no differences.
+
+# print command is a method that helps to do sth.
+print(my_variable) # print accepst a variable
+
+# printing  value directly
+print("hello world")
+print(1233)
+
+
