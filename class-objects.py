a3e85b131ca652d20981768eb7b856a7fa33c84f
mamanipatricia <patttymq@gmail.com>
2020-03-02 12:38:07 -0400

    part6. class - objects sudnet and methods

--- class-objects.py
+++ class-objects.py
@@ -21,3 +21,19 @@ print(player_one.name == player_two.name)
 print(player_one.numbers == player_two.numbers)
 
 
+# a student example
+
+class Student:
+    def __init__(self, name, school):
+        self.name = name
+        self.school = school
+        self.marks = []
+    def average(self):
+        return sum(self.marks) / len(self.marks)
+
+anna = Student("Anna", 'MIT')
+anna.marks.append(56)
+anna.marks.append(71)
+
+print(anna.marks)
+print(anna.average())
