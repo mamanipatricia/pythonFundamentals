40143c3e2f4e4db96ad75a74b3e92028147ac814
mamanipatricia <patttymq@gmail.com>
2020-03-02 13:22:54 -0400

    part7. @classmethod and @staticmethod

--- class-objects.py
+++ class-objects.py
@@ -31,9 +31,31 @@ class Student:
     def average(self):
         return sum(self.marks) / len(self.marks)
 
+    # def go_to_school(self):
+    #     print("I am going to going to school" school")
+    @classmethod
+    # when you want to have a method that it is global..
+    def go_to_school(cls):
+        print("I am going to school")
+        print("I am a {}".format(cls))
+
+    @staticmethod
+    # when you want to have a method that it is global..
+    def go_to_school():
+        print("I am going to school")
+
 anna = Student("Anna", 'MIT')
+rolf = Student("rolf", 'Oxford')
+
 anna.marks.append(56)
 anna.marks.append(71)
 
+anna.go_to_school()
+
+Student.go_to_school() # this can be declared with both classmethod and staticmethod
+# all objects ANNA, ROLF share the same method go_to_school
+
 print(anna.marks)
 print(anna.average())
+# anna.go_to_school() = object.method
+# Class method and static method
